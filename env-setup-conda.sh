ea9e328071ab68506e9a68ec9f89d5256a03c87d
guilhemheinrich <zezongo@gmail.com>
2023-05-16 13:33:07 +0200

    Updated config

--- env-setup-conda.sh
+++ env-setup-conda.sh
@@ -11,13 +11,12 @@
 nameEnv=csvFormater
 
 conda create --name ${nameEnv} python=3.6 --yes
-conda install --name ${nameEnv} pip
 
 # using pip inside our environment : conda doesn't handle well the dependencies/version without gpu
 # pip reference here the envornment version, so don't use pip3
 conda activate ${nameEnv}
 pip install --upgrade pip
-pip install pyyaml
-pip install pylint
+pip install pyyaml pylint argparse datetime uuid
+
 
 echo "Use conda activate: ${nameEnv} or conda deactivate to get out of this environnment"
